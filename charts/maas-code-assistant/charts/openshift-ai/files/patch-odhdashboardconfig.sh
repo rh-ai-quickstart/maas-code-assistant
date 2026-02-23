1e01515a1714a1eca4010e813853a475e3b2cc1c
James Harmison <jharmison@redhat.com>
2026-02-23 09:49:31 -0500

    fix: have patch force a rollout of the dashboard to trigger maas interface

--- charts/maas-code-assistant/charts/openshift-ai/files/patch-odhdashboardconfig.sh
+++ charts/maas-code-assistant/charts/openshift-ai/files/patch-odhdashboardconfig.sh
@@ -3,3 +3,6 @@
 set -ex
 
 oc patch odhdashboardconfig odh-dashboard-config --patch-file=odhdashboardconfig.yaml --type=merge
+
+oc rollout restart deployment/rhods-dashboard
+oc rollout status deploy/rhods-dashboard
