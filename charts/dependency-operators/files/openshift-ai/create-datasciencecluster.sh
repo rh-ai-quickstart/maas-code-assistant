8531eb037beb2c702655f95e799ccb4863e83e28
James Harmison <jharmison@redhat.com>
2026-06-15 10:59:27 -0400

    fix: add a wait/workaround for cnpg timing issue with initdb

--- charts/dependency-operators/files/openshift-ai/create-datasciencecluster.sh
+++ charts/dependency-operators/files/openshift-ai/create-datasciencecluster.sh
@@ -8,6 +8,9 @@ oc apply -f gatewayclass.yaml
 oc apply -f gateway.yaml
 {{- with $db := .Values.postgresCluster }}
 {{- if $db.create }}
+
+oc rollout status -n cloudnative-pg deployment/cnpg-controller-manager
+sleep 5
 oc apply -f cluster.yaml
 while ! [ "$(oc get cluster -n {{ $db.namespace }} {{ $db.name }} -o jsonpath='{.status.readyInstances}')" -eq "{{ $db.instances | default 1 }}" ]; do
   sleep 5
@@ -16,6 +19,7 @@ uri=$(oc get secret -n {{ $db.namespace }} {{ $db.name }}-app -ojsonpath='{.data
 oc create secret generic maas-db-config -n redhat-ods-applications --from-literal=DB_CONNECTION_URL="$uri" --dry-run=client -oyaml | oc apply -f-
 {{- end }}
 {{- end }}
+
 while ! oc apply -f datasciencecluster.yaml; do
   sleep 5
 done
